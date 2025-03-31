9bb16edaa35519f1e51e8a8f36ad7bdf269470e0
WandrewRicardo <wandrewricardo@gmail.com>
2025-03-31 12:35:27 -0300

    Add files via upload

---
+++ ControleEstoque/src/main/java/br/com/controleestoque/controleestoque/TiposdeClientes.java
@@ -0,0 +1,19 @@
+
+package br.com.controleestoque.controleestoque;
+//construtor classe tipodeclientes
+public class TiposdeClientes {
+    private int telefone;
+    private String nome;
+    private boolean PJ;
+    private int ident;
+        public TiposdeClientes(String nomedocliente){
+            nome = nomedocliente;
+        }
+    
+}
+  
+    
+    
+
+    
+
